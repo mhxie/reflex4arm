7f0fb2e07712c7256b9174e222aece86a1c95b6f
Minghao Xie <mhxie@ucsc.edu>
2023-05-09 20:29:34 +0000

    added a new log

--- nvme/nvme_sw_table.c
+++ nvme/nvme_sw_table.c
@@ -74,14 +74,19 @@ int nvme_sw_table_push_back(struct nvme_sw_table *t, long fg_handle,
     uint32_t key = RTE_PER_LCORE(cpu_nr) + fg_handle
                    << 5 + t->queue_tail[fg_handle] << 17;
     int ret = rte_hash_add_key_data(t->table, (void *)&key, (void *)ctx);
+    if (unlikely(t->total_request_count >= NVME_SW_TABLE_SIZE)) {
+        printf("push_back ERROR: Cannot push more requests into the table\n");
+        return RET_NOMEM;
+    } else {
+        printf("push_back OKAY: found the request in the table\n");
+        printf("fg_handle = %ld, queue_head = %d\n", fg_handle,
+               t->queue_head[fg_handle]);
+    }
+
     t->total_token_demand[fg_handle] += ctx->req_cost;
 
     t->queue_tail[fg_handle]++;
     t->total_request_count++;
-    if (unlikely(t->total_request_count >= NVME_SW_TABLE_SIZE)) {
-        printf("push_back ERROR: Cannot push more requests into the table\n");
-        return RET_NOMEM;
-    }
     if (unlikely(t->queue_tail[fg_handle] >= NVME_SW_TABLE_SIZE)) {
         t->queue_tail[fg_handle] = 0;
         t->queue_overflow_count[fg_handle]++;
